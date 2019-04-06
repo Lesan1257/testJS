6037843fb3d5a51088b8519bb7393059b6045ca1
Dima <lesanbsuir@gmail.com>
2019-04-06 19:30:08 +0300

    Мой третий комит

--- lesson1/main.js
+++ lesson1/main.js
@@ -1,26 +1,23 @@
 'use strict'
-// js 
-var time = prompt('Введите дату в формате YYYY-MM-DD', 'YYYY-MM-DD');
-var money = +prompt('Ваш бюджет на месяц?', 100);
 
-console.log(time);
-console.log(money);
+let money = prompt("Ваш бюджет на месяц?", ''),
+time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
-var appData = {
-     cash: money,
-     timeData: time,
-     optionalExpenses: {},
-     savings: false,
-     income: []
-}
-var answer1 = prompt('как дела?');
+let appData = {
+budget: money,
+expenses: {},
+optionalExpenses: {},
+income: [],
+timeData: time,
+savings: false
+};
 
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+a2 = prompt("Во сколько обойдется?", ''),
+a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+a4 = prompt("Во сколько обойдется?", '');
 
-appData.expenses = {
-     answer1: prompt('Привет')
-}
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
 
-alert('Ваш бюджет на 1 день = ' + (money) / 30 + ' рубля(ей)');
-
-
-console.log(appData);
+alert(appData.budget / 30);
