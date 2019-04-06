a5676525bbc46d14fb9f8b764bba1c01f55388f1
Dima <lesanbsuir@gmail.com>
2019-04-06 19:09:10 +0300

    Мой первый комит

---
+++ lesson1/main.js
@@ -0,0 +1,26 @@
+'use strict'
+
+var time = prompt('Введите дату в формате YYYY-MM-DD', 'YYYY-MM-DD');
+var money = +prompt('Ваш бюджет на месяц?', 100);
+
+console.log(time);
+console.log(money);
+
+var appData = {
+     cash: money,
+     timeData: time,
+     optionalExpenses: {},
+     savings: false,
+     income: []
+}
+var answer1 = prompt('как дела?');
+var answer2 = prompt('Привет');
+
+appData.expenses = {
+   answer1: answer2
+}
+
+alert('Ваш бюджет на 1 день = ' + (money)/30 + ' рубля(ей)');
+
+
+console.log(appData);
